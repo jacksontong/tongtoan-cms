a8275d86cddf4c3bb75e6b3a3596aedf51aa5b97
Jackson Tong <cauvongkhuyetbs@gmail.com>
2014-07-18 22:57:47 +0700

    List all user and pagination admin cpanel

--- application/modules/admin/controllers/user.php
+++ application/modules/admin/controllers/user.php
@@ -1,5 +1,5 @@
 <?php 
-class User extends AdminController 
+class User extends Admin_Controller 
 {
 	function __construct() 
 	{
@@ -9,8 +9,24 @@ class User extends AdminController
 
 	public function index()
 	{
+		$this->load->model('Muser');
+
+		// Pagination
+		$config['base_url'] = base_url().'admin/user/index/';
+		$config['total_rows'] = $this->Muser->countAll();
+		$config['per_page'] = 3; 
+		$config['uri_segment'] = 4;
+		$this->load->library('pagination', $config);
+		$this->_data['page_link']=$this->pagination->create_links();
+
+		// Fetch all users
+		$start=$this->uri->segment(4);
+		$this->_data['info']=$this->Muser->listAllUser($config['per_page'],$start);
+
+		// Load view
 		$this->_data['titlePage'] = "List All User";
 		$this->_data['loadPage']="user/index_view";
 		$this->load->view($this->_data['path'], $this->_data);
 	}
+
 }
--- application/modules/admin/views/user/index_view.php
+++ application/modules/admin/views/user/index_view.php
@@ -1,1 +1,36 @@
-<h1>Hello Admin Module - User controller - Index Action</h1>
+<table align="center" width="450">
+	<tr>
+    	<td colspan="6"><strong><a href="<?php echo base_url()."$module/user/add"; ?>"><font color="#99CC33">Add A User</font></a></strong></td>
+    </tr>
+	<tr>
+    	<td class="title">STT</td>
+        <td class="title">Username</td>
+        <td class="title">Email</td>
+        <td class="title">Level</td>
+        <td class="title">Edit</td>
+        <td class="title">Del</td>
+    </tr>
+    <?php 
+    	$stt=0;
+    	foreach ($info as $item) {
+    		$stt++;
+    		echo '<tr>';
+    		echo "<td>$stt</td>";
+    		echo "<td>$item[username]</td>";
+    		echo "<td>$item[email]</td>";
+    		if ($item['level'] == 1) {
+    			echo "<td>Member</td>";
+    		} else {
+    			echo "<td><font color='red'>Admin</font></td>";
+    		}
+    		echo "<td><a href='".base_url()."$module/user/edit/$item[id]'>Edit</a></td>";
+    		echo "<td><a href='".base_url()."$module/user/del/$item[id]'>Del</a></td>";
+    		echo '</tr>';
+    	}
+    ?>
+    
+</table>
+<br>
+<?php 
+	echo $page_link;
+?>
