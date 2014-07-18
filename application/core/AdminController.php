261467135eae5b666fe7277310ebba2e99b067a7
Jackson Tong <cauvongkhuyetbs@gmail.com>
2014-07-18 21:22:34 +0700

    Autoload master layout

---
+++ application/core/AdminController.php
@@ -0,0 +1,12 @@
+<?php 
+class AdminController extends MY_Controller 
+{
+	protected $_data;
+	public function __construct()
+	{
+		parent::__construct();
+		$mod = $this->uri->segment(1);
+		$this->_data['module'] = $mod;
+		$this->_data['path'] = "$mod/template";
+	}
+}
